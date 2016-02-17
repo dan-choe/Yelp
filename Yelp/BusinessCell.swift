2ecbb75425935e4cccd582ec0ff32ffb68508de0
Dan Choe <dan.choe@stonybrook.edu>
2016-02-16 19:27:11 -0500

    Updated to show the list of the data

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,49 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Admin on 2/16/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewLabel: UILabel!
+    
+    var business: Business!{
+        didSet{
+            nameLabel.text = business.name
+            addressLabel.text = business.address
+            distanceLabel.text = business.distance
+            categoryLabel.text = business.categories
+            reviewLabel.text = "\(business.reviewCount) Reviews"
+            
+            thumbImageView.setImageWithURL(business.imageURL!)
+            ratingImageView.setImageWithURL(business.ratingImageURL!)
+        }
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
