7011d4f08ff7abe18fa93146114f1c98921d5380
Dan Choe <dan.choe@stonybrook.edu>
2016-02-20 17:55:02 -0500

    Update Search Function

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,21 +8,43 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//UISearchResultsUpdating
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UIScrollViewDelegate {
 
-    @IBOutlet weak var tableView: UITableView!
-    var businesses: [Business]!
+    @IBOutlet weak var searchBar: UISearchBar!
+    var searchController: UISearchController!
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var businesses: [Business]!
+    var filteredData: [Business]!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = 120
+        searchBar.delegate = self
 
-        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+        
+        //searchBar = UISearchBar()
+        
+
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableViewAutomaticDimension
+        navigationItem.titleView = searchBar
+        
+        
+        
+        
+        // Sets this view controller as presenting view controller for the search interface
+        definesPresentationContext = true
+        
+        Business.searchWithTerm("Food", completion: { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
+            self.filteredData = businesses
             self.tableView.reloadData()
         
             for business in businesses {
@@ -31,6 +53,10 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
             }
         })
 
+        
+        
+
+        
 /* Example of Yelp search with more search options specified
         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
@@ -49,8 +75,8 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        if businesses != nil{
-            return businesses.count
+        if filteredData != nil{
+            return filteredData.count
         }else{
             return 0
         }
@@ -58,17 +84,72 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
     // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         
         let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
         
-        cell.business = businesses[indexPath.row]
+        //cell.business = businesses[indexPath.row]
+        cell.business = filteredData[indexPath.row]
         
         return cell
     }
-
-
+    
+    /*
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = searchText.isEmpty ? filteredData : businesses.filter({(dataItem: Business) -> Bool in
+            return dataItem.name!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+        })
+        self.tableView.reloadData()
+    }
+    */
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        if searchText.isEmpty {
+            filteredData = businesses
+        } else {
+            // The user has entered text into the search box
+            // Use the filter method to iterate over all items in the data array
+            // For each item, return true if the item should be included and false if the
+            // item should NOT be included
+            filteredData = searchText.isEmpty ? filteredData : businesses.filter({(dataItem: Business) -> Bool in
+                // If dataItem matches the searchText, return true to include it
+                if dataItem.name!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil {
+                    return true
+                } else {
+                    return false
+                }
+            })
+        }
+        tableView.reloadData()
+    }
+    
+    
+    
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+        self.searchBar.endEditing(true)
+        self.searchBar.resignFirstResponder()
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        self.searchBar.endEditing(true)
+        self.searchBar.resignFirstResponder()
+        //self.tableView.reloadData()
+    }
+    
+ 
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        self.searchBar.endEditing(true)
+        self.searchBar.resignFirstResponder()
+    }
+    
+  
+    
     /*
     // MARK: - Navigation
 
